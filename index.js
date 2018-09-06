65fd2ef1503b2d906e5a706f23b7ff10091e61d9
Dong Yuwei <newdongyuwei@gmail.com>
2018-09-06 18:44:58 +0800

    apply alias to use external global libs

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HelloMessage from "./hello_message";
 
-import bar from "./bar";
-
-bar();
-
 const mountNode = document.getElementById("app");
 ReactDOM.render(<HelloMessage name="world" />, mountNode);
