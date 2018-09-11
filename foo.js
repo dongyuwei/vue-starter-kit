8b5ab9b175de747062ff2e26014fd67473fcd620
Dong Yuwei <newdongyuwei@gmail.com>
2018-09-11 22:19:11 +0800

    vue

--- foo.js
+++ foo.js
@@ -6,5 +6,5 @@ export default (a, b) => {
   return a + b;
 };
 
-export const foo2 = "foo foo";
+export const foo2 = `foo foo`;
 export const data = { [foo2]: "bar" };
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloMessage from "./hello_message";
+import Vue from "vue";
 
 const mountNode = document.getElementById("app");
 ReactDOM.render(<HelloMessage name="world" />, mountNode);
+
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>{{ todo.text }}</li>",
+});
+
+new Vue({
+  el: "#vue-app",
+  data: {
+    groceryList: [{ id: 0, text: "Vegetables" }, { id: 1, text: "Cheese" }, { id: 2, text: "Whatever else humans are supposed to eat" }],
+  },
+});
